60ad5836b0dc98020ae8b6eed43deb59cdc610d6
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2025-06-18 15:59:35 -0500

    Create run-user-script-v3.js

---
+++ run-user-script-v3.js
@@ -0,0 +1,26 @@
+//all of these functions exist in the file to which this is imported to
+(async () => {
+  try{
+    // @ts-ignore
+    const scheduler = new TaskScheduler();
+    // @ts-ignore
+    const store = new DataStore();
+    // @ts-ignore
+    const context = new Context(canvas, container, scheduler, store);
+    // @ts-ignore
+    const gestureDetector = new GestureDetector(context); 
+    // @ts-ignore
+    registerDrawLoop(context);
+    console.log(canvas, container, scheduler, store, context, gestureDetector);
+
+    //GENERATED CODE STARTS HERE
+
+    buildSketch(context, canvas, scheduler, store, gestureDetector);
+
+    //GENERATED CODE ENDS HERE
+
+    scheduler.start();
+  } catch (error) {
+    console.warn(error);
+  }
+})();
